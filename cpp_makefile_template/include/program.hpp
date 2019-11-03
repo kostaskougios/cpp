dcc26e9a7e79cd8d775196927e09cb8217cf30b8
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-11-03 17:11:07 +0000

    template project

---
+++ cpp_makefile_template/include/program.hpp
@@ -0,0 +1,17 @@
+#ifndef PROGRAM_HPP
+#define PROGRAM_HPP
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+namespace program
+{
+    inline void foo()
+    {
+        cout << "program::foo()\n";
+    }
+}
+
+#endif
