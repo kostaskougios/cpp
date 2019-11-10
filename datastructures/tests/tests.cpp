a80d58c1b6695e3ff972d48f2d122a75bfd2c1f5
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-11-10 18:35:02 +0000

    -

--- datastructures/tests/tests.cpp
+++ datastructures/tests/tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "person.hpp"
 #include <immer/map.hpp>
+#include <immer/set.hpp>
 
 using namespace model;
 
@@ -13,6 +14,12 @@ TEST(MAP_OF_PERSON_VALUE, Positive) {
     auto v0 = immer::map<std::string, Person>().set("kostas", Person("Kostas"));
 
     ASSERT_EQ(v0["kostas"],Person("Kostas"));
+}
+
+TEST(SET_OF_PERSON, Positive) {
+    auto v0=immer::set<Person>().insert(Person("kostas")).insert(Person("nick")).insert(Person("kostas"));
+    ASSERT_EQ(v0.size(),2);
+    ASSERT_TRUE(v0.count(Person("kostas"))==1);
 }
 
 TEST(MAP_OF_PERSON_KEY, Positive) {
