0052b8f7bf1c43f1b1b7f0df7f9ceadd2fda4518
kostas.kougios@gmail.com <kostas.kougios@googlemail.com>
2019-11-30 21:36:19 +0000

    -

--- sfml/include/renderer.hpp
+++ sfml/include/renderer.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "game.hpp"
+#include <range/v3/all.hpp>
 
 namespace game
 {
@@ -16,7 +17,8 @@ public:
     Renderer(sf::RenderWindow *window, Game game) : w(window)
     {
         auto &state = game.getState();
-        };
+        ranges::for_each(state, [&](Object &o) { o.render(window); });
+    };
 };
 
 } // namespace game
